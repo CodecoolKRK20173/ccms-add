c7761197f236ba08a450392ee4838ac25d9274c2
Szwajcii <d.szwajkos@gmail.com>
2018-06-12 10:47:19 +0200

    Implement new classes.

--- src/main/java/controller/ManagerController.java
+++ src/main/java/controller/ManagerController.java
@@ -1,4 +1,29 @@
 package controller;
 
 public class ManagerController extends UserController {
+
+
+    public ManagerController(User loggedUser) {
+        super(loggedUser);
+    }
+
+    public void addMentor() {
+
+    }
+
+    public void removeMentor() {
+
+    }
+
+    public void editMentor() {
+
+    }
+
+    public List<User> listStudents() {
+
+    }
+
+    public List<User> listMentors() {
+
+    }
 }
--- src/main/java/controller/MentorController.java
+++ src/main/java/controller/MentorController.java
@@ -1,4 +1,39 @@
 package controller;
 
 public class MentorController extends UserController {
+
+
+    public MentorController(User loggedUser) {
+        super(loggedUser);
+    }
+
+
+    public void addStudent() {
+
+    }
+
+    public void removeStudent() {
+
+    }
+
+    public void editStudent() {
+
+    }
+
+    public List<User> listStudents() {
+
+    }
+
+    public void addAssignment() {
+
+    }
+
+    public void gradeAssignment() {
+
+    }
+
+    public void checkAttendence() {
+
+    }
+
 }
