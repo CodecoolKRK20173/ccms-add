01430985a890ccef5e9a36b73e0c35e8ffc43ce0
Szwajcii <d.szwajkos@gmail.com>
2018-06-12 11:10:42 +0200

    Implement new class.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,18 @@
 package controller;
 
+import model.DataHandler;
+
 public class Controller {
+
+    private DataHandler dataHandler;
+    private View view;
+    private UserController userController;
+
+
+    public Controller(DataHandler dataHandler, View view) {
+        this.dataHandler = new DataHandler();
+        this.view = new View();
+    }
+
+    
 }
