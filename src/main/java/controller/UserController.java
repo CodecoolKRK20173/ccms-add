26fd7beaaa0eef058a6c50c52d29d0f28a10bbda
Szwajcii <d.szwajkos@gmail.com>
2018-06-12 10:12:07 +0200

    Implement new abstract class

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import java.util.List;
+
 public abstract class UserController {
+
+    private List<String> menu;
+    private User loggedUser;
+
+
+    public UserController(User loggedUser) {
+        this.loggedUser = loggedUser;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void handleMenu() {
+        
+    }
+
 }
