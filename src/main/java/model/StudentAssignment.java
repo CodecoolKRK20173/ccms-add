cbba4b1d2fe89ef4f2ea1df420f63934edeb5e9c
Cyan <dawid.grygier5@gmail.com>
2018-06-12 10:39:11 +0200

    Added classes for model package

--- src/main/java/model/StudentAssignment.java
+++ src/main/java/model/StudentAssignment.java
@@ -1,4 +1,36 @@
 package model;
 
 public class StudentAssignment {
+
+    private Assignment assignment;
+    private String link;
+    private int grade;
+
+    public StudentAssignment (Assignment assignment){
+        this.assignment = assignment;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
