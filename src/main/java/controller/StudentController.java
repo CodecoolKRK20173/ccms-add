f1d6efc4cde643a0e63f6b9a2383d5effb29cd11
Szwajcii <d.szwajkos@gmail.com>
2018-06-12 10:34:05 +0200

    Implement new class

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,4 +1,21 @@
 package controller;
 
+import java.util.List;
+import model.StudentAssignment;
+
 public class StudentController extends UserController {
+
+
+    public StudentController(User loggedUser) {
+        super(loggedUser);
+    }
+
+    public void submitAssignment() {
+
+    }
+
+    public List<StudentAssignment> listAssignment() {
+
+    }
+
 }
