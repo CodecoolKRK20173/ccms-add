43afdd40d1657bef2d239b4c542e239cc499a730
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-06-13 23:16:53 +0200

    Change call of application, main loop and user loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,30 +9,10 @@ public class Main {
     private static FileHandler fileHandler;
 
     public static void main(String[] args) {
-        View view = new View();
-        Controller mainController = new Controller();
-        DataHandler dataHandler = mainController.getDataHandler();
-        fileHandler = new FileHandler(dataHandler);
+
+        String filename = "src/main/resources/usersData.txt";
         
-        //test user - first usage of application
-        if (dataHandler.getManagerList().size() == 0) {
-            Manager manager = new Manager("test","test","test","test");
-            dataHandler.addUser(manager);
-        }
-                
-        //read from file
-        fileHandler.readFromFile(mainController);
-        mainController.getView().printUserList(fileHandler.getDataHandler().getManagerList());
-        
-        User loggedUser = mainController.checkLogin(mainController.getDataHandler());
-        UserController userController = new ManagerController(loggedUser, view);
-        
-        Integer menuItem = -1;
-        while (menuItem != 0) {
-            view.printMenu(userController.getMenu());
-            menuItem = view.getAnswerAsInt("Option: ");
-            userController.handleMenu(mainController.getDataHandler(), menuItem);
-        }
-        fileHandler.saveToFile();
+        Controller mainController = new Controller(filename);
+        mainController.execute();
     }
 }
