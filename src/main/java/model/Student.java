1f72da27ac033d1883b6c767d5ac10694775ced9
Szwajcii <d.szwajkos@gmail.com>
2018-06-13 14:47:14 +0200

    Add attendance hashMap to student

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,15 +1,19 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Student extends User {
 
     private List<StudentAssignment> assignmentList;
+    private Map<String, String> studentAttendance;
 
     public Student(String login, String password, String name, String surname) {
         super(login, password, name, surname);
         this.assignmentList = new ArrayList<StudentAssignment>();
+        this.studentAttendance = new HashMap<>();
     }
 
     public List<StudentAssignment> getAssignmentList() {
