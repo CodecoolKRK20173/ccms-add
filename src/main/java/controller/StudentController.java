b5c8534e07e04185a9f4d7623c3d0e84ef659301
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-06-13 23:21:16 +0200

    Add submitAssignment() and refactoring

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -8,23 +8,42 @@ import java.util.List;
 public class StudentController extends UserController {
 
     {
-        menu = Arrays.asList("View my grades", "Submit assignment", "Exit CcMS");
+        menu = Arrays.asList("View assignments", 
+                            "Submit assignment");
     }
 
-    public StudentController(User loggedUser, View view) {
-        super(loggedUser, view);
+    public StudentController(User loggedUser, DataHandler dataHandler, View view) {
+        super(loggedUser, dataHandler, view);
     }
 
+    public void listStudentAssignments() {
+        view.printStudentAssignmentList(((Student) loggedUser).getAssignmentList());
+    }
 
     public void submitAssignment() {
+        listStudentAssignments();
 
+        String assignmentId = view.getAnswerAsString("Type assignment's id: ");
+        Assignment assignment = dataHandler.getAssignmentById(assignmentId);
+        String link = view.getAnswerAsString("Type link: ");
+            
+        dataHandler.submitAssignment((Student) loggedUser, assignment, link);
     }
 
-    public void listAssignment(List<StudentAssignment> studentAsssignmentList) {
-        view.printAssignmentList(studentAsssignmentList);
-    }
+    public void handleMenu(Integer number) {
 
-    public void handleMenu(DataHandler dataHandler, Integer number) {
-        
+        switch (number) {
+            // 1 "View assignments"
+            case 1:
+                listStudentAssignments();
+                break;
+            // 2 "Submit assignment"
+            case 2:
+                submitAssignment();
+                break;
+            
+            default:
+                break;
+        }
     }
 }
